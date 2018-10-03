220ccb79aee2cb130dd1a34a2a9f1fd022fb20c6
Neeraj Kumar Maurya <36784272+1720mca1054@users.noreply.github.com>
2018-10-03 23:33:58 +0530

    userInput

---
+++ Day-1/User-input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Hello1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner userInputScanner=new Scanner(System.in);
+		System.out.print("What is your name?\n");
+		String userInputName=userInputScanner.nextLine();
+		System.out.println("Hello "+ userInputName +"!");
+	}
+
+}
