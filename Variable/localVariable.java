66bb07ba2415f16b6cfd241e5bca308fbb044513
Neeraj Kumar Maurya <36784272+1720mca1054@users.noreply.github.com>
2018-10-05 22:14:19 +0530

    local Variable

---
+++ Variable/localVariable.java
@@ -0,0 +1,14 @@
+
+public class localVariable {
+	public void pupAge() {
+	      int age = 0;
+	      age = age + 7;
+	      System.out.println("Puppy age is : " + age);
+	   }
+
+	   public static void main(String args[]) {
+	      localVariable test = new localVariable();
+	      test.pupAge();
+	   }
+
+}
