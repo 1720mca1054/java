acbe175364a84fd8dba0a9c527bc4b6f59a8fe2d
Neeraj Kumar Maurya <36784272+1720mca1054@users.noreply.github.com>
2018-11-08 23:46:05 +0530

    Update Fibonacci.java

--- Fibonacci/Fibonacci.java
+++ Fibonacci/Fibonacci.java
@@ -1,6 +1,6 @@
 public class Fibonacci {
  
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
         int n = 10, t1 = 0, t2 = 1;
         System.out.print("First " + n + " terms: "); 
