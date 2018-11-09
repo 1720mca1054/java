d2555e468c5b16c03891c70ccd2369ecf4ae56b2
Neeraj Kumar Maurya <36784272+1720mca1054@users.noreply.github.com>
2018-11-09 23:51:41 +0530

    Update User-input.java

--- Day-1/User-input.java
+++ Day-1/User-input.java
@@ -3,7 +3,6 @@ public class Hello1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		Scanner userInputScanner=new Scanner(System.in);
 		System.out.print("What is your name?\n");
 		String userInputName=userInputScanner.nextLine();
